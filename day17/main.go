6ce3109224fa5ec7ccfef71d303c45615ccf5e7f
roberto montalti <roberto.montalti00@gmail.com>
2022-12-18 23:44:41 +0100

    Gotta fix part 2

--- day17/main.go
+++ day17/main.go
@@ -22,16 +22,16 @@ var rocks [][]string = [][]string{
 	{"##", "##"},
 }
 
-func checkCollision(chamber [][]rune, rock []string, x, y uint64) bool {
-	rockH := uint64(len(rock))
-	rockW := uint64(len(rock[0]))
+func checkCollision(chamber [][]rune, rock []string, x, y int) bool {
+	rockH := len(rock)
+	rockW := len(rock[0])
 
-	if rockW+x-1 >= uint64(len(chamber[y])) || rockH+y-1 >= uint64(len(chamber)) {
+	if rockW+x-1 >= len(chamber[y]) || rockH+y-1 >= len(chamber) {
 		return true
 	}
 
-	for ry := uint64(0); ry < rockH; ry++ {
-		for rx := uint64(0); rx < rockW; rx++ {
+	for ry := 0; ry < rockH; ry++ {
+		for rx := 0; rx < rockW; rx++ {
 			if chamber[y+ry][x+rx] == '#' && rock[ry][rx] == '#' {
 				return true
 			}
@@ -41,10 +41,10 @@ func checkCollision(chamber [][]rune, rock []string, x, y uint64) bool {
 	return false
 }
 
-func tryMove(chamber [][]rune, rock []string, x, y uint64, dir rune) uint64 {
+func tryMove(chamber [][]rune, rock []string, x, y int, dir rune) int {
 	switch dir {
 	case '<':
-		if x > 0 && !checkCollision(chamber, rock, x-1, y) {
+		if x - 1 >= 0 && !checkCollision(chamber, rock, x-1, y) {
 			return x - 1
 		}
 	case '>':
@@ -56,12 +56,12 @@ func tryMove(chamber [][]rune, rock []string, x, y uint64, dir rune) uint64 {
 	return x
 }
 
-func drawRock(chamber [][]rune, rock []string, x, y uint64) {
-	rockH := uint64(len(rock))
-	rockW := uint64(len(rock[0]))
+func drawRock(chamber [][]rune, rock []string, x, y int) {
+	rockH := len(rock)
+	rockW := len(rock[0])
 
-	for ry := uint64(0); ry < rockH; ry++ {
-		for rx := uint64(0); rx < rockW; rx++ {
+	for ry := 0; ry < rockH; ry++ {
+		for rx := 0; rx < rockW; rx++ {
 			if chamber[y+ry][x+rx] == '.' && rock[ry][rx] == '#' {
 				chamber[y+ry][x+rx] = '#'
 			}
@@ -69,23 +69,30 @@ func drawRock(chamber [][]rune, rock []string, x, y uint64) {
 	}
 }
 
-func clearChamber(chamber [][]rune) [][]rune {
+func clearChamber(chamber *[][]rune) {
     top := "......."
-    for i := 0; i < len(chamber); i++ {
-        if string(chamber[i]) != top {
-            top = string(chamber[i])
+    for i := 0; i < len(*chamber); i++ {
+        if string((*chamber)[i]) != top {
+            top = string((*chamber)[i])
             break
         }
     }
-    for i := 0; i < len(chamber); i++ {
-        chamber[i] = []rune(".......")
+    for i := 0; i < len(*chamber); i++ {
+        (*chamber)[i] = []rune(".......")
     }
-    chamber[len(chamber)-1] = []rune(top)
-    return chamber
+    (*chamber)[len(*chamber)-1] = []rune(top)
+}
+
+func initChamber(height int) (chamber [][]rune) {
+	for i := 0; i < height - 1; i++ {
+		chamber = append(chamber, []rune("......."))
+	}
+	chamber = append(chamber, []rune("#######"))
+    return
 }
 
 type StartingIds struct {
-    ri, ii  uint64
+    ri, ii  int
     topRock string
 }
 
@@ -93,56 +100,29 @@ type PatternData struct {
     placed, height uint64
 }
 
-func problem(input string, maxplacements uint64) uint64 {
-	var chamber [][]rune
-	for i := uint64(0); i < 512; i++ {
-		chamber = append(chamber, []rune("......."))
-	}
-	chamber = append(chamber, []rune("#######"))
+func p1(input string, maxplacements int) int {
+    chamber := initChamber(maxplacements * 4)
 
-	ii := uint64(0)
-	ri := uint64(0)
-	placed := uint64(0)
-    totalheight := uint64(0)
-    heightWithPatterns := uint64(0)
-	height := uint64(len(chamber) - 1)
-
-	patterns := make(map[StartingIds]PatternData)
-    patternFound := false
+	ii := 0
+	ri := 0
+	placed := 0
+	height := len(chamber) - 1
 
 	for placed < maxplacements {
-		ri = ri % uint64(len(rocks))
+		ri = ri % len(rocks)
 		rock := rocks[ri]
-		rockH := uint64(len(rock))
+		rockH := len(rock)
 
-        if !patternFound {
-            sp := StartingIds{ri, ii, string(chamber[height])}
-            if patterns[sp].placed == 0 {
-                patterns[sp] = PatternData{placed, height}
-            } else {
-                fmt.Println("pattern starts at", patterns[sp].placed)
-                patternFound = true
-                pdiff := uint64(placed - patterns[sp].placed)
-                hdiff := uint64(patterns[sp].height - height)
-                q := uint64(maxplacements) / pdiff
-                q--
-                placed += pdiff * q
-                heightWithPatterns = hdiff*q + patterns[sp].height
-                totalheight += heightWithPatterns
-                fmt.Println(q, pdiff, hdiff)
-            }
-        }
-
-		x := uint64(2)
+		x := 2
 		y := height - (3 + rockH)
-		for ; y+rockH < uint64(len(chamber)); y++ {
-			ii = ii % uint64(len(input))
+		for ; y+rockH < len(chamber); y++ {
+			ii = ii % len(input)
 			gasdir := input[ii]
 			x = tryMove(chamber, rock, x, y, rune(gasdir))
+			ii++
 			if checkCollision(chamber, rock, x, y+1) {
 				break
 			}
-			ii++
 		}
 
 		drawRock(chamber, rock, x, y)
@@ -151,21 +131,92 @@ func problem(input string, maxplacements uint64) uint64 {
 			height = y
 		}
 
+		ri++
+		placed++
+	}
+
+    return len(chamber) - height - 1
+}
+
+func p2(input string, maxplacements uint64) uint64 {
+    chamber := initChamber(100)
+
+	ii := 0
+	ri := 0
+	placed := uint64(0)
+    patternFound := false
+	height := len(chamber) - 1
+    patternsHeight := uint64(0)
+    incrementalHeight := uint64(0)
+
+    patterns := make(map[StartingIds]PatternData)
+
+    flush := func() (uint64, int) {
         if height <= 8 {
-            chamber = clearChamber(chamber)
-            totalheight += uint64(len(chamber)) - height - 1
-            height = uint64(len(chamber) - 1)
+            tmp := uint64(len(chamber) - 1 - height)
+            clearChamber(&chamber)
+            return incrementalHeight + tmp, len(chamber) - 1
         }
+        return incrementalHeight, height
+    }
+
+	for {
+		ri = ri % len(rocks)
+		rock := rocks[ri]
+		rockH := len(rock)
+
+        if !patternFound {
+            sp := StartingIds { ri, ii, string(chamber[height]) }
+            fmt.Println(sp)
+            patternFound = patterns[sp].placed != 0
+            if !patternFound {
+                patterns[sp] = PatternData { placed, incrementalHeight }
+            } else {
+                patternData := patterns[sp]
+                placedDiff := placed - patternData.placed
+                heightDiff := incrementalHeight - patternData.height
+                q := maxplacements / placedDiff
+                patternsHeight = patternData.height + heightDiff * (q - 1)
+                placed = patternData.placed + placedDiff * (q - 1)
+
+                fmt.Println(placedDiff, heightDiff, q, patternsHeight, placed, incrementalHeight, patternData.height, patternData)
+            }
+        }
+
+        if uint64(placed) >= maxplacements  {
+            break
+        }
+
+		x := 2
+		y := height - (3 + rockH)
+		for ; y+rockH < len(chamber); y++ {
+			ii = ii % len(input)
+			gasdir := input[ii]
+			ii++
+
+			x = tryMove(chamber, rock, x, y, rune(gasdir))
+			if checkCollision(chamber, rock, x, y+1) {
+				break
+			}
+		}
+
+		if y < height {
+			height = y
+		}
+
+		drawRock(chamber, rock, x, y)
+
+        incrementalHeight, height = flush()
 
 		ri++
 		placed++
 	}
 
-	return totalheight
+    return patternsHeight + incrementalHeight
 }
 
 func main() {
 	input := strings.TrimSpace(readFile("./input2.txt"))
-	fmt.Println(problem(input, 2022))
-	fmt.Println(problem(input, 1000000000000))
+	fmt.Println(p1(input, 2022))
+	fmt.Println(p2(input, 1000000000000))
 }
